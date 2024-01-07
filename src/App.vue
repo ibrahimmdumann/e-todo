bd748a6dcc90513d384e42888f5da332fe274661
ibrahimduman <ibrahim.duman@roofstacks.com>
2024-01-07 23:03:17 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Vue Todo Uygulaması</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Yeni görev ekle..." />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">Sil</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
